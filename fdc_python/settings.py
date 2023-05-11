ea923589717e6d0ac16e92f071cbb78f16b4938a
RicardoGoncal <ricardo.studo@gmail.com>
2023-05-11 08:31:42 -0300

    atualizacao codigos

--- fdc_python/settings.py
+++ fdc_python/settings.py
@@ -24,6 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "fdcapp/templates")
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- fdcapp/views.py
+++ fdcapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from re import template
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+# from blog_app.models import Post, Comment
+# from blog_app.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = "fdcapp/homepage.html"
+
 
 # Create your views here.
