b24e7db4e5c266ba729afa2c00736ca469775989
RicardoGoncal <ricardo.studo@gmail.com>
2023-05-12 16:18:54 -0300

    teste

--- fdc_python/settings.py
+++ fdc_python/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import pymysql
 from pathlib import Path
 import os
 
@@ -78,9 +78,15 @@ WSGI_APPLICATION = 'fdc_python.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            "read_default_file": "/home/fdc/my.cnf",
+        },
     }
 }
 
@@ -124,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Login redirect
 LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- fdc_python/urls.py
+++ fdc_python/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fdcapp.urls')),
-    path('', views.LoginView.as_view(template_name='registration/login.html'), name='login')
+    path('accounts/login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
